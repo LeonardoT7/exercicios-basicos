368883f3af223d1be43f2c5d3e543e3cead508f3
Leonardo Treichel <leonardotreichel7@gmail.com>
2024-03-28 17:48:20 -0300

    Validador de cálculo extremamente básico

--- validador-calculo.py
+++ validador-calculo.py
@@ -2,28 +2,28 @@
 n1 = float (input('Digite o primeiro número: '))
 n2 = float (input('Digite o segundo número: '))
 
-#Estrutura condicional para validar o resultado da soma
+#Estrutura condicional para validar o resultado da soma.
 resposta = float(input(f'Informe o resultado da operação: {n1} + {n2} = '))
 if resposta == n1 + n2:
     print('Você acertou! ')
 else:
     print('Você errou! O resultado correto é: ', {n1 + n2})
 
-#Estrutura condicional para validar o resultado da subtração
+#Estrutura condicional para validar o resultado da subtração.
 resposta = float(input(f'Informe o resultado da operação: {n1} - {n2} = '))
 if resposta == n1 - n2:
     print('Você acertou! ')
 else:
     print('Você errou! O resultado correto é: ', {n1 - n2})
 
-#Estrutura condicional para validar o resultado da multiplicação
+#Estrutura condicional para validar o resultado da multiplicação.
 resposta = float(input(f'Informe o resultado da operação: {n1} * {n2} = 5'))
 if resposta == n1 * n2:
     print('Você acertou! ')
 else:
     print('Você errou! O resultado correto é: ', {n1 * n2})
 
-#Estrutura condicional para validar o resultado da divisão
+#Estrutura condicional para validar o resultado da divisão.
 resposta = float(input(f'Informe o resultado da operação: {n1} / {n2} = '))
 if resposta == n1 / n2:
     print('Você acertou! ')
